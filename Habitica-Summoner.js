ba704f2f353ca60f61100cc60611f781012ec3a9
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2025-04-19 16:39:16 -0400

    Reorganize the code

--- Habitica-Summoner.js
+++ Habitica-Summoner.js
@@ -7,20 +7,26 @@ const HabiticaXClient = process.env.HABITICA_X_CLIENT
 // Todoist Auth
 const todoistApiKey = process.env.TODOIST_API_KEY
 
-// Set up Habitica API URL
-const habiticaStatusURL = 'https://habitica.com/api/v3/status'
-const habiticaTaskURL = 'https://habitica.com/api/v3/tasks/user?type=todos'
+// API Base URLs
+const habiticaBaseUrl = 'https://habitica.com/api/v3'
+const todoistBaseUrl = 'https://api.todoist.com/rest/v2'
+
+// Habitica API URL Calls
+const habiticaStatusURL = `${habiticaBaseUrl}/status`
+const habiticaTaskURL = `${habiticaBaseUrl}/tasks/user?type=todos`
 const habiticaUrl = habiticaTaskURL
+
+// Todoist API URL Calls
+const todoistUrl = `${todoistBaseUrl}/tasks`
+
+// Habatica API Headers
 const habiticaHeaders = {
    'x-api-user': habiticaUserId,
    'x-api-key': habiticaApiToken,
    'x-client': HabiticaXClient,
 }
 
-// Todoist
-
-const todoistUrl = new URL('https://api.todoist.com/rest/v2/tasks')
-
+// Todoist API Headers
 const todoistHeaders = {
    Authorization: `Bearer ${todoistApiKey}`,
    'Content-Type': 'application/json',
